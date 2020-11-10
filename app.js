cf780d5fbc4e23591580081df8a6f86c00dc51f5
Mark Grebe <grebe@ucmo.edu>
2020-11-09 19:03:03 -0600

    Add POST

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 const routes = require('./routes')
+const bodyParser = require('body-parser');
 
 //Database connection
 //app.use(function(req, res, next){
@@ -15,6 +16,9 @@ const routes = require('./routes')
 //    res.locals.connection.connect();
 //    next();
 //});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('client'))
 
--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-// get user lists
+// get car list
 router.get('/cars', function(req, res) {
   let sql = `SELECT * FROM car`;
   db.query(sql, function(err, data, fields) {
@@ -15,5 +15,19 @@ router.get('/cars', function(req, res) {
     res.json(data)
     })
   });
+
+// create new car
+router.post('/cars', function(req, res) {
+  let sql = `INSERT INTO car(make, model, year) VALUES (?)`;
+  let values = [
+    req.body.make,
+    req.body.model,
+    req.body.year    
+  ];
+  db.query(sql, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.json(data)
+  })
+});
 
 module.exports = router;
